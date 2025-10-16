c2a25de1c71fc85f6a77078ea87a156361cd3a33
mikias-tulu <miki.tulu@gmail.com>
2025-10-17 01:41:09 +0300

    Update README and add favicon and manifest files; enhance contact form success feedback

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -10,6 +10,14 @@ class MyDocument extends Document {
           <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
           {/* meta end */}
 
+          {/* favicon begin */}
+          <link rel="apple-touch-icon" sizes="180x180" href="/img/photo/favicon_io/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/img/photo/favicon_io/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/img/photo/favicon_io/favicon-16x16.png" />
+          <link rel="manifest" href="/img/photo/favicon_io/site.webmanifest" />
+          <link rel="shortcut icon" href="/img/photo/favicon_io/favicon.ico" />
+          {/* favicon end */}
+
           {/* public assets begin */}
           <link rel="stylesheet" href="/css/plugins/bootstrap-grid.css" />
           <link rel="stylesheet" href="/css/plugins/font-awesome.min.css" />
